9fccdb5060f991c0fd42b6889e68d2b46b4a4f12
dila93 <sebas.dila@gmail.com>
2016-11-23 13:08:11 -0500

    Update multi.sh

--- Segundo Parcial/src_mpi_cpu/multi.sh
+++ Segundo Parcial/src_mpi_cpu/multi.sh
@@ -6,5 +6,10 @@
 #SBATCH --ntasks-per-node 8
 #sbatch --gres=gpu:1
 
+/usr/local/bin/mpicxx -o multi.o -c multi_mpi.c
+
+"/usr/local/cuda-8.0"/bin/nvcc -ccbin g++ -m64 -gencode arch=compute_30,code=sm_30 -gencode arch=compute_35,code=sm_35 -gencode arch=compute_37,code=sm_37 -ge$
+
+/usr/local/bin/mpicxx -o multi multi.o multi_cuda.o  -L"/usr/local/cuda-8.0"/lib64 -lcudart
 
 mpirun multi
